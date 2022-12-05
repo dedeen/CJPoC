5afa11d0619794e9475bec8e0f7b332e2680a37a
dedeen <dan@dsblue.net>
2022-12-05 02:37:08 +0000

    4

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Script to install terraform and other utilities.
-# dan@dsblue.net 
+# dan@dsblue.net  sddsdssd
 #
 # jq = JSON parser / wget = web file getter 
 function install_prerequisites() {
