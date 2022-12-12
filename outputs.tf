34d686a423c576aea1b81a1b1cf4d2957eabdec1
dedeen <115123138+dedeen@users.noreply.github.com>
2022-12-11 23:48:44 -0600

    ongoing dev

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+# Output from vpc module 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "vpc_public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "vpc_private_subnets" {
+  value = module.vpc.private_subnets
+}
