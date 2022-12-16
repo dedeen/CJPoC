e630640e8e2b152e24d45631ea53fee7e4800f16
dedeen <115123138+dedeen@users.noreply.github.com>
2022-12-15 21:53:38 -0600

    ongoing dev

--- outputs.tf
+++ outputs.tf
@@ -1,28 +1,43 @@
-# Get handles for the VPC elements created for downstream provisioning
-#
-#     vpcs built with for_each indexed here as xxxxxxx
+#    Output form vpc creation. datacenter1 and datacenter2 are the VPCs created.
+#    Here we get handles to build the rest of the infrastructure. 
+#    To do - get clever and loop over the datacenters from the map in vars file. 
 
-output "dc1-vpc_id" {
-  description = "vpc"
+
+output "vpc_dc1-vpc_id" {
   value = module.vpc["datacenter1"].vpc_id
 }
-output "dc1-vpc_arn" {
+output "vpc_dc1-vpc_arn" {
   value = module.vpc["datacenter1"].vpc_arn
 }
-output "dc1-vpc_cidr" {
+output "vpc_dc1-vpc_cidr" {
   value = module.vpc["datacenter1"].vpc_cidr_block
 }
-output "dc1-vpc_private_snet0" {
+output "vpc_dc1-vpc_private_snet0" {
   value = module.vpc["datacenter1"].private_subnets[0]
 }
-output "dc1-vpc_public_snet0" {
+output "vpc_dc1-vpc_public_snet0" {
   value = module.vpc["datacenter1"].public_subnets[0]
 }
-output "dc1-vpc_intra_snet0" {
+output "vpc_dc1-vpc_intra_snet0" {
   value = module.vpc["datacenter1"].intra_subnets[0]
 }
 
-output "dc2-vpc_id" {
-  description = "vpcs created"
+#
+output "vpc_dc2-vpc_id" {
   value = module.vpc["datacenter2"].vpc_id
+}
+output "vpc_dc2-vpc_arn" {
+  value = module.vpc["datacenter2"].vpc_arn
+}
+output "vpc_dc2-vpc_cidr" {
+  value = module.vpc["datacenter2"].vpc_cidr_block
+}
+output "vpc_dc2-vpc_private_snet0" {
+  value = module.vpc["datacenter2"].private_subnets[0]
+}
+output "vpc_dc2-vpc_public_snet0" {
+  value = module.vpc["datacenter2"].public_subnets[0]
+}
+output "vpc_dc2-vpc_intra_snet0" {
+  value = module.vpc["datacenter2"].intra_subnets[0]
 }
