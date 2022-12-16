7c0abaa2c4445c1219b719aa0b860985d94d79ec
dedeen <115123138+dedeen@users.noreply.github.com>
2022-12-15 18:45:29 -0600

    ongoing dev

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,28 @@
-/*
-output "test" {
-  description = "ID of VPCs Created"
-  value = {
-    for i in module.vpc:
-      value = [ for new_vpcs in vpc : oregon_dcs.vpc_id]
-  }
+# Get handles for the VPC elements created for downstream provisioning
+#
+#     vpcs built with for_each indexed here as xxxxxxx
+
+output "dc1-vpc_id" {
+  description = "vpc"
+  value = module.vpc["datacenter1"].vpc_id
+}
+output "dc1-vpc_arn" {
+  value = module.vpc["datacenter1"].vpc_arn
+}
+output "dc1-vpc_cidr" {
+  value = module.vpc["datacenter1"].vpc_cidr_block
+}
+output "dc1-vpc_private_snet0" {
+  value = module.vpc["datacenter1"].private_subnets[0]
+}
+output "dc1-vpc_public_snet0" {
+  value = module.vpc["datacenter1"].public_subnets[0]
+}
+output "dc1-vpc_intra_snet0" {
+  value = module.vpc["datacenter1"].intra_subnets[0]
 }
 
-*/
+output "dc2-vpc_id" {
+  description = "vpcs created"
+  value = module.vpc["datacenter2"].vpc_id
+}
