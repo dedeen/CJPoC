6b77681d6c7c1ec4673e39dca4d117ea5ca76d40
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-18 14:07:57 -0600

    Create ssh-attributes.bat

---
+++ ssh-attributes.bat
@@ -0,0 +1,7 @@
+:Windows batch file to change attributes on AWS keypair.pem file so it 
+:  can be used to ssh from windows to EC2. 
+:  You need to change the file name listed here as an example 
+:
+icacls terraform-key-pair.476d.pem /reset
+icacls terraform-key-pair.476d.pem /grant:r "$($env:Admin):(r)"
+>icacls terraform-key-pair.476d.pem /grant:r %username%:(R)
