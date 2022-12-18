b789f2f7e408a3d144f8103750d599c55b3f9ffc
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-18 16:54:22 -0600

    added path to bastion hust into private subnet

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "vpc" {
     single_nat_gateway      = true
 }
 
-# Create SecGrp to allow ICMP into the public subnet
+# Create SecGrp to allow ICMP into attached subnet
 resource "aws_security_group" "allow_inbound_icmp" {
   name          = "allow_inbound_icmp"
   description   = "ICMP inbound"
@@ -46,7 +46,7 @@ resource "aws_security_group" "allow_inbound_icmp" {
   }
 }
 
-# Create SecGrp to allow all IPv4 traffic into public subnet
+# Create SecGrp to allow all IPv4 traffic into attached subnet
 resource "aws_security_group" "allow_ipv4" {
   name                  = "allow_ipv4"
   description           = "All inbound v4"
@@ -71,7 +71,32 @@ resource "aws_security_group" "allow_ipv4" {
   }
 }
 
-# Create EC2 Instance(s) in the public subnet
+# Create SecGrp to allow inbound ssh, outbound all 
+resource "aws_security_group" "allow_ssh" {
+  name                  = "allow_ssh"
+  description           = "All inbound ssh"
+  vpc_id                = module.vpc["datacenter1"].vpc_id
+  ingress {
+    description         = "All inbound ssh"
+    cidr_blocks         = ["0.0.0.0/0"]
+    from_port           = 22
+    to_port             = 22
+    protocol            = "tcp"
+  }
+  egress {
+    description         = "All outbound v4"
+    cidr_blocks         = ["0.0.0.0/0"]
+    from_port           = 0
+    to_port             = 0
+    protocol            = "-1"
+  }
+  tags = {
+    Name = "allow_ssh"
+    Owner = "dan-via-terraform"
+  }
+}
+
+# Create EC2 Instance(s) in the public subnet - allow inbound icmp and other ipv4
 resource "aws_instance" "ec2-public-subnet" {
     ami                                 = "ami-094125af156557ca2"
     instance_type                       = "t2.micro"
@@ -79,11 +104,26 @@ resource "aws_instance" "ec2-public-subnet" {
     associate_public_ip_address         = true
     subnet_id                           = module.vpc["datacenter1"].public_subnets[0]
     vpc_security_group_ids              = [aws_security_group.allow_inbound_icmp.id, aws_security_group.allow_ipv4.id]
-    #security_groups                    = ["allow_ipv4", "allow_inbound_icmp"]
     source_dest_check                   = false
     tags = {
           Owner = "dan-via-terraform"
           Name  = "ec2-inst1-public"
     }
 }
-                                                       
+ 
+# Create EC2 Instance(s) in the private subnet 
+#    No public IP, so must connect through an instance in the public subnet, ssh only, outbound v4 allowed 
+resource "aws_instance" "ec2-private-subnet" {
+    ami                                 = "ami-094125af156557ca2"
+    instance_type                       = "t2.micro"
+    key_name                            = "${aws_key_pair.generated_key.key_name}"
+    associate_public_ip_address         = false
+    subnet_id                           = module.vpc["datacenter1"].private_subnets[0]
+    vpc_security_group_ids              = [aws_security_group.allow_ssh]
+    source_dest_check                   = false
+    tags = {
+          Owner = "dan-via-terraform"
+          Name  = "ec2-inst1-public"
+    }
+}
+ 
